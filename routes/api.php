e08bfebb59213891e12480f302b2b2d8c5628b41
mateus <122534003+MateusQa@users.noreply.github.com>
2026-02-08 09:16:42 -0300

    Feat: api routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\Response;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/', function (Request $request) {
+    return response()->json([
+        'message' => 'Bem-vindo à API do Projeto Filament!',
+        'status' => 'success',
+    ]);
+});
